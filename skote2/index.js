4416d53821f889a64375f3b2ad35f83cae8c130f
DanilTezin <78168662+DanilTezin@users.noreply.github.com>
2021-11-15 17:25:14 +0300

    Add files via upload

---
+++ skote2/index.js
@@ -0,0 +1,8 @@
+let panel = document.getElementById("panel");
+
+function vis(){
+    let drop = document.getElementById("drop");
+    if(drop.style.visibility == "visible") drop.style.visibility = "hidden"
+    else drop.style.visibility = "visible"
+    
+}
