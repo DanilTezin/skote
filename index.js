ac96f941e37d8e1c51f7a861ddeaab4814fa8e10
DanilTezin <78168662+DanilTezin@users.noreply.github.com>
2021-11-17 21:10:28 +0500

    17.11

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 let panel = document.getElementById("panel");
-
 function vis(){
     let drop = document.getElementById("drop");
     if(drop.style.visibility == "visible") drop.style.visibility = "hidden"
-    else drop.style.visibility = "visible"
-    
+    else drop.style.visibility = "visible"  
+}
+let panel1 = document.getElementById("panel1");
+function vis1(){
+    let drop1 = document.getElementById("drop1");
+    if(drop1.style.visibility == "visible") drop1.style.visibility = "hidden"
+    else drop1.style.visibility = "visible"  
 }
