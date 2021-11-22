13853b39a05db11b044455bd92005831c1dc6eda
DanilTezin <78168662+DanilTezin@users.noreply.github.com>
2021-11-22 23:32:45 +0500

    add textarea

--- index.js
+++ index.js
@@ -10,3 +10,38 @@ function vis1(){
     if(drop1.style.visibility == "visible") drop1.style.visibility = "hidden"
     else drop1.style.visibility = "visible"  
 }
+
+var lastMode = "";
+
+let textTextArea = '';
+let textAreaMode = '';
+let lastButton;
+let textArea = document.getElementById("txtarea")
+
+function setModTextArea(id){
+    document.getElementById(id).value = document.getElementById(id).value += "<strong>" + "</strong>"
+}
+
+function setValueTextArea(event,mode){
+    if(mode == 'clear'){
+        document.execCommand(lastMode)
+        lastButton.style.backgroundColor = "white"
+        lastButton.style.color = "black"
+        lastButton = ''
+        lastMode = ''
+        return 0
+    }else{
+        document.execCommand(mode);
+    document.execCommand(lastMode)
+    lastMode = mode;
+    console.log(lastMode)
+    if(lastButton){
+        lastButton.classList.remove('active')     
+       lastButton.style.backgroundColor = "white"
+    }
+    event.target.style.backgroundColor = "#bfbfbf"
+    event.target.style.color = "black"
+    lastButton = event.target
+    }
+    
+}
